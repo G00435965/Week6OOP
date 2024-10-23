4322534c16b1d79ada069b772fdd121bec9e2ff0
KEELANMURPHY-STUDENT <g00435965@atu.ie>
2024-10-23 17:00:43 +0100

    Added trycatch

--- src/main/java/ie/atu/week6/UsingTryCatch.java
+++ src/main/java/ie/atu/week6/UsingTryCatch.java
@@ -6,16 +6,20 @@ public class UsingTryCatch {
     public static void main(String[] args) {
         System.out.println("Please enter a number between 0 - 9:");
         Scanner myscan = new Scanner(System.in);
+        try {
+            String input = myscan.nextLine();
 
-        String input = myscan.nextLine();
+            int myNum = Integer.parseInt(input);
 
-        int myNum = Integer.parseInt(input);
-
-        if (myNum >= 0 && myNum <= 9) {
-            System.out.println("You entered " + myNum);
-        }
-        else {
-            System.out.println("Not a valid number");
+            if (myNum >= 0 && myNum <= 9) {
+                System.out.println("You entered " + myNum);
+            } else {
+                System.out.println("Not a valid number");
+            }
+        } catch (NumberFormatException ae) {
+            System.out.println("Input is not a valid number");
+        } finally {
+            myscan.close();
         }
     }
 }
